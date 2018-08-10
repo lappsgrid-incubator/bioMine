b4c4243bc3f0508b94b0c1dff258efb320a13a36
root <root@biomine2.openstacklocal>
2018-08-10 11:32:40 -0400

    Fix pid file location

--- post-pmc.sh
+++ post-pmc.sh
@@ -4,5 +4,5 @@ set -eu
 for file in `cat index.txt` ; do
     echo "Posting $file"
     curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' http://localhost:8080/biomine/indexer/index/path/{doc}?collection=literature\&path=$file
-    sleep 5
+    sleep 10
 done
